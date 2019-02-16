faeb6f9cc5d268d9a0c1904d6896db1b0b94d843
sebastiencegepmatane <sebastienducegep@gmail.com>
2019-02-16 00:46:28 -0500

    Camera FPS 120

--- src/main/java/frc/robot/interaction/Camera.java
+++ src/main/java/frc/robot/interaction/Camera.java
@@ -1,6 +1,7 @@
 package frc.robot.interaction;
 
 import edu.wpi.cscore.UsbCamera;
+import edu.wpi.cscore.VideoMode;
 import edu.wpi.first.cameraserver.CameraServer;
 import frc.robot.RobotMap;
 
@@ -10,13 +11,18 @@ public class Camera implements RobotMap.Camera {
 
     public Camera(){
         camera = CameraServer.getInstance().startAutomaticCapture();
-        camera.setFPS(IMAGE_PAR_SECONDE);
-        camera.setResolution(LARGEUR, HAUTEUR);
+        
+        //camera.setFPS(IMAGE_PAR_SECONDE);
+        //camera.setResolution(LARGEUR, HAUTEUR);
+        
         //camera.setExposureManual(0);
         //camera.setWhiteBalanceManual(0);
         //camera.setPixelFormat(PixelFormat.kMJPEG);
         //camera.setExposureAuto();
         //camera.setWhiteBalanceAuto();
+        
+        //camera.setVideoMode(VideoMode.PixelFormat.kMJPEG,1280,720,60);
+        camera.setVideoMode(VideoMode.PixelFormat.kMJPEG,640,480,120);
         
     }
 }
